823beaf5fbf15b41e07b24357a7025cfff28a26a
Yingying Yao <67403094+YingyingYao@users.noreply.github.com>
2021-04-29 21:05:15 +0800

    Update Classify.py

--- Classify.py
+++ Classify.py
@@ -1,18 +1,9 @@
-from sklearn.ensemble import StackingClassifier
-from sklearn.svm import SVC
-from sklearn.ensemble import (RandomForestClassifier, AdaBoostClassifier,GradientBoostingClassifier, ExtraTreesClassifier)
-estimators = [('rf_model',RandomForestClassifier()),
-              ('ab_model', AdaBoostClassifier()),
-              ('ex_model',ExtraTreesClassifier()),
-              ('gdbc_model',GradientBoostingClassifier()), 
-              ('svc_model',SVC())
-]
-clf = StackingClassifier(
-    estimators=estimators, final_estimator=SVC(probability=True),cv=10
-)
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(
-    new_data, Y, test_size=0.2)
-clf.fit(X_train, y_train)
-score=clf.score(X_test, y_test)
-print(score)
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import VotingClassifier
+clf1=GaussianNB()
+clf2=LogisticRegression()
+vote_clf=VotingClassifier(estimators = [('NB',clf1),('LOG',clf2)],voting='soft')
+for clf in (clf1,clf2,vote_clf):
+    score=cross_val_score(clf,X,Y,cv=10).mean()
+    print(clf.__class__.__name__,score)
