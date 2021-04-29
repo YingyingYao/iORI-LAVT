d393954b2bf9c6704462b8a498cd625d8a8bbac4
Yingying Yao <67403094+YingyingYao@users.noreply.github.com>
2021-04-29 21:03:24 +0800

    Update Feature_selection.py

--- Feature_selection.py
+++ Feature_selection.py
@@ -1,6 +1,6 @@
 import numpy as np
-from sklearn.linear_model import ElasticNet,ElasticNetCV
-enet = ElasticNetCV(alphas=np.array([0.01, 0.02, 0.03,0.04, 0.05, 0.06, 0.07, 0.08,0.09, 0.1]),l1_ratio=[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]).fit(X,Y)
-mask = enet.coef_ != 0
+from sklearn.linear_model import Lasso,LassoCV
+model_lasso=LassoCV(cv=5, alphas=np.array([0.001,0.002,0.003,0.004, 0.005, 0.006, 0.007, 0.008,0.009, 0.01])).fit(X, Y)
+model_lasso.fit(X,Y)
+mask = model_lasso.coef_ != 0 
 new_data = X[:,mask]
-print(new_data.shape)
